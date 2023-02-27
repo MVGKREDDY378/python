bebedeac01512d36744ebe560d96087459a697f5
MVGKREDDY378 <126455303+MVGKREDDY378@users.noreply.github.com>
2023-02-27 14:42:39 +0530

    Add files via upload

---
+++ greastest of 3 numbers.py
@@ -0,0 +1,9 @@
+a=int(input("enter the a value:"))
+b=int(input("enter the b value:"))
+c=int(input("enter the c value:"))
+if a>b and a>c:
+    print ("a is max")
+elif b>c:
+  print("b is max")
+else :
+ print ("c is max")
