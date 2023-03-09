6de864d8ca0664ea2679261f438a39e3f46b72a1
MVGKREDDY378 <126455303+MVGKREDDY378@users.noreply.github.com>
2023-03-09 17:46:50 +0530

    Update instance and class .py

--- instance and class .py
+++ instance and class .py
@@ -1,16 +1,16 @@
-class Student:
+class student:
     school_name='abc school'
     def _init_(self,name,age):
         self.name=name
         self.age=age
     def show(self):
-        print('student',self.name,self.age,Student.school_name)
+        print('student',self.name,self.age,student.school_name)
     def change_age(self,new_age):
         self.new_age
     def modify_school_name(cls,new_name):
         cls.schoolname=new_name
-    s1=Student("harry",12)
+    s1=student("harry",12)
     s1.show()
     s1.change_age(14)
-    Student.modify_school_name('xyzschool')
+    student.modify_school_name('xyzschool')
     s1.show
